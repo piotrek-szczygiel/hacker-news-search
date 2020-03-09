8d93fa60d096b56c0f85c5dceee55629e155ab7f
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-03-09 16:10:51 +0100

    Add favicon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod story;
 
+use actix_files::NamedFile;
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use story::Story;
 
@@ -12,11 +13,16 @@ async fn index() -> impl Responder {
     }
 }
 
+#[get("/favicon.ico")]
+async fn favicon() -> impl Responder {
+    NamedFile::open("./favicon.ico")
+}
+
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     pretty_env_logger::init();
 
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(index).service(favicon))
         .bind("127.0.0.1:3232")?
         .run()
         .await
